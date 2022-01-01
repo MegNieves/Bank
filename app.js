0ad4f904782e791f092d045d5bbfb90c45ad39ac
Meghan Nieves <megnieves12@gmail.com>
2022-01-01 18:52:16 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import './myConnection'; // Database Connection
+import express from 'express';
+import swaggerUIExpress from 'swagger-ui-express';
+import swaggerDocs from './apiDocs/swagger.json';
+import routes from './routes/index';
+
+const app = express();
+
+app.use('/api-docs', swaggerUIExpress.serve, swaggerUIExpress.setup(swaggerDocs));
+app.use('/api', routes);
+
+export default app;
