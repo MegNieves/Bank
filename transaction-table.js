f6e67c1899949e03864e1fbfbc285f4b4ab559cf
Meghan Nieves <megnieves12@gmail.com>
2022-01-01 18:56:38 -0500

    Create transaction-table.js

---
+++ transaction-table.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('transaction', { 
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true
+      },
+      userId: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      accountNumber: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Account number field cannot be empty'
+          }
+        }
+      },
+      amount: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Amount field cannot be empty'
+          }
+        }
+      },
+      transactionType: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Transaction type field cannot be empty'
+          }
+        }
+      },
+      referenceNumber: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Reference number type field cannot be empty'
+          }
+        }
+      },
+      accountName: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: 'Account name field cannot be empty'
+          }
+        }
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+       });
+   
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('transaction');
+  }
+};
